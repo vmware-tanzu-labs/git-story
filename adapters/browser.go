a342a606ef9426cf69ed7cffc675dea0c6ddfa50
Liam Morley <liam@carpeliam.com>
2021-05-05 17:47:54 -0400

    Remove extraneous print statements

--- adapters/browser.go
+++ adapters/browser.go
@@ -1,7 +1,6 @@
 package adapters
 
 import (
-	"fmt"
 	"os/exec"
 
 	"github.com/git-story-branch/git-story-branch/usecases"
@@ -10,12 +9,9 @@ import (
 type Browser struct{}
 
 func (browser Browser) OpenURL(url string) (*exec.Cmd, error) {
-	fmt.Println("url")
-	fmt.Println(url)
 	cmd := exec.Command("open", url)
 	error := cmd.Start()
 	return cmd, error
-	// return .Start()
 }
 
 func NewBrowser() usecases.Browser {
